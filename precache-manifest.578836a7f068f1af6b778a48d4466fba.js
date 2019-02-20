e3fc95a13b4d294a6ee5eff097ca51ed885efa45
vladimircreative <vladimir.creative@yandex.ru>
2019-02-20 11:26:33 +0300

    initial

---
+++ precache-manifest.578836a7f068f1af6b778a48d4466fba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6c935a2c7b750104db1",
+    "url": "/star-wars/static/css/main.c90965d9.chunk.css"
+  },
+  {
+    "revision": "d6c935a2c7b750104db1",
+    "url": "/star-wars/static/js/main.d6c935a2.chunk.js"
+  },
+  {
+    "revision": "0e03336767f8793a3f55",
+    "url": "/star-wars/static/js/runtime~main.0e033367.js"
+  },
+  {
+    "revision": "4dcc6123d6b2cd6f16df",
+    "url": "/star-wars/static/js/2.4dcc6123.chunk.js"
+  },
+  {
+    "revision": "8c15550237b3fcb43362061280a976dc",
+    "url": "/star-wars/static/media/back.8c155502.jpg"
+  },
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/star-wars/static/media/STARWARS.35f31c7a.woff"
+  },
+  {
+    "revision": "4863eeab2bacf57e99f46c8cc3d84d42",
+    "url": "/star-wars/index.html"
+  }
+];
